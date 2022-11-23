9b4788c3e6f340d8efa7142bc9f4fb3e87bd3542
suxin <suxinwork@aliyun.com>
2022-11-23 15:56:41 +0800

    chore: 修改读取逻辑

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/23 下午3:08
+# @Author  : Xsu
+# @File    : __init__.py
--- main.py
+++ main.py
@@ -9,46 +9,61 @@ import imapclient
 import re
 from typing import List
 
+OUTLOOK = "outlook"
+GOOGLE = "google"
+OAUTH_MAPPING  = {
+    OUTLOOK:{"host":"outlook.office365.com","port":993}
+}
+QUERY_FIELDS = ["Inbox","Junk","Archive","Deleted","Drafts","Notes","Outbox","Sent"]
+
 class MailParse:
 
-    def __init__(self,parsetype:str,emails:List):
-        self.parseType = parsetype
-        self.emails = emails
-        self.mappingDic = {"outlook":{"host":"outlook.office365.com","port":993}}
+    def __init__(self,parsetype:str):
+        if parsetype not in OAUTH_MAPPING.keys():
+            raise ValueError(f"parseType should be set in \f{list(OAUTH_MAPPING.keys())}")
+        self.host = OAUTH_MAPPING.get(parsetype).get('host')
+        self.port = OAUTH_MAPPING.get(parsetype).get('port')
+        self.imapObj = imapclient.IMAPClient(self.host, port=self.port)
 
-    def login(self):
-        if self.parseType not in self.mappingDic.keys():
-            return False
+    def login(self,email:str,password:str):
         try:
-            self.imapObj = imapclient.IMAPClient(self.mappingDic.get(self.parseType).get("host"), port=self.mappingDic.get(self.parseType).get("port"))
+            self.imapObj.login(email,password)
             return True
         except:
             return False
 
-    def parse(self,filed:List,from_user:str,email_subject:str,re_content:str):
-        parse_result = []
-        for email_object in self.emails:
-            try:
-                if self.login() != True:
+    def parse_batch(self,filed:List,from_user:str,email_objs:List,email_subject:str,re_content:str,re_idx:int):
+        response = []
+        for email_obj in email_objs:
+           result = self.parse(filed,from_user,email_obj.get("username"),email_obj.get("password"),email_subject,re_content,re_idx)
+           if result:
+               response.append({"email":email_obj,"parse_result":result})
+        return response
+
+    def parse(self,filed:List,from_user:str,email:str,password:str,email_subject:str,re_content:str,re_idx:int):
+        if not set(filed).issubset(QUERY_FIELDS):
+            raise ValueError(f"filed should be set in \f{QUERY_FIELDS}")
+        if not self.login(email,password):
+            raise ValueError(f"LOGIN failed \f{email}")
+        try:
+            for box in filed:
+                self.imapObj.select_folder(box, readonly=False)
+                uids = self.imapObj.search(['FROM', from_user])  # UNSEEN
+                if len(uids) < 1:
                     continue
-                self.imapObj.login(email_object["username"], email_object["password"])
-                for box in filed:
-                    self.imapObj.select_folder(box, readonly=False)
-                    uids = self.imapObj.search(['FROM', from_user])  # UNSEEN
-                    if len(uids) < 1:
+                for uid in uids:
+                    Rawmessages = self.imapObj.fetch(uid, [b'BODY[]'])
+                    messages = pyzmail.PyzMessage.factory(Rawmessages[uid][b'BODY[]'])
+                    emailtitle = messages.get_subject()
+                    if emailtitle != email_subject:
                         continue
-                    for uid in uids:
-                        Rawmessages = self.imapObj.fetch(uid, [b'BODY[]'])
-                        messages = pyzmail.PyzMessage.factory(Rawmessages[uid][b'BODY[]'])
-                        emailtitle = messages.get_subject()
-                        if emailtitle == email_subject:
-                            result = re.findall(re_content, messages.html_part.get_payload().decode('utf-8'))[1]
-                            parse_result.append({"account":email_object["username"],"result":result})
-            except Exception as e:
-                print(f"error-{email_object['username']}-{e}")
-        return parse_result
+                    content = messages.html_part.get_payload().decode('utf-8')
+                    return re.findall(re_content,content)[re_idx]
+        except Exception as e:
+            raise ValueError(f"parse failed {email}:{e}")
 
 
 if __name__ == '__main__':
-    print(MailParse("outlook",[{"username":"","password":""}]).parse(["Inbox","Junk"],'community@minima.global','Welcome to the Minima Incentive program',r"<a.+?href=\"(.+?)\".*>"))
-
+    # print(MailParse("outlook").login("",""))
+    print(MailParse("outlook").parse(["Inbox","Junk"],"community@minima.global","","",'Welcome to the Minima Incentive program',
+                                     r"<a.+?href=\"(.+?)\".*>",1))
